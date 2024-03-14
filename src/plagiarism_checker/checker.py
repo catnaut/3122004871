d17c6aba0b7122b30e8fc8555bbd7334683484a9
catnaut <catnaut@outlook.com>
2024-03-14 11:46:29 +0800

    feat(checker): add tfidf and cosine similarity

---
+++ src/plagiarism_checker/checker.py
@@ -0,0 +1,17 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def cal_tf_idf(texts: list):
+    vectorizer = TfidfVectorizer()
+    tf_idf = vectorizer.fit_transform([" ".join(text) for text in texts])
+    return tf_idf
+
+
+def get_vector():
+    pass
+
+
+def cal_cosine_similarity(vectors):
+    similarity_matrix = cosine_similarity(vectors)
+    return similarity_matrix
