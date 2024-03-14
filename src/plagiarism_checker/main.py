4904bacb6607978f1da5d568c7f9bc1c11eafe3f
catnaut <catnaut@outlook.com>
2024-03-14 12:50:06 +0800

    style(main): 增强可读性

--- src/plagiarism_checker/main.py
+++ src/plagiarism_checker/main.py
@@ -1,24 +1,31 @@
 import argparse
-import checker,pre
+import checker, pre
 import time
 from io import TextIOWrapper
 
 parser = argparse.ArgumentParser(description="simple python plagiarism checker")
 
-parser.add_argument('origin_file', type=argparse.FileType('r',encoding='utf-8'), help='原文件路径')
-parser.add_argument('check_file',type=argparse.FileType('r',encoding='utf-8'),help='检查文件路径')
-parser.add_argument('output_file', type=argparse.FileType('w',encoding='utf-8'), help='输出文件路径')
+parser.add_argument(
+    "origin_file", type=argparse.FileType("r", encoding="utf-8"), help="原文件路径"
+)
+parser.add_argument(
+    "check_file", type=argparse.FileType("r", encoding="utf-8"), help="检查文件路径"
+)
+parser.add_argument(
+    "output_file", type=argparse.FileType("w", encoding="utf-8"), help="输出文件路径"
+)
 
 
-def output_result(output_file:TextIOWrapper, result:float):
-    '''输出结果到文件和控制台'''
-    output = round(result,2)
+def output_result(output_file: TextIOWrapper, result: float):
+    """输出结果到文件和控制台"""
+    output = round(result, 2)
     print(output)
     try:
         output_file.write(str(output))
     except Exception as e:
         print(e)
         print("输出文件失败")
+
 
 def main():
     try:
@@ -29,16 +36,17 @@ def main():
         print("error")
         print(e)
 
-
     origin = args.origin_file.read()
     check = args.check_file.read()
     output = args.output_file
-    texts = [origin,check]
+    texts = [origin, check]
+    # 预处理
     texts = [pre.data_preprocessing(text) for text in texts]
+    # 计算 tf-idf
     tf_idf = checker.cal_tf_idf(texts)
     similarity_matrix = checker.cosine_similarity(tf_idf)
-    # print(similarity_matrix[0][1])
-    output_result(output,similarity_matrix[0][1])
+    # 输出结果
+    output_result(output, similarity_matrix[0][1])
 
 
 if __name__ == "__main__":
